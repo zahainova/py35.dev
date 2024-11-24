270b61ddb509056d9a5c17170c3b13b4a22b3d6b
zahainova <yul.taranenko@gmail.com>
2024-11-24 11:00:37 +0200

    added main

---
+++ Lecture-01/exersices/08.py
@@ -0,0 +1,9 @@
+days = int(input("Days:"))
+hours = int(input("Hours:"))
+minutes = int(input("Minutes:"))
+seconds = int(input("Seconds:"))
+
+total_seconds = (days * 24 * 60 *60) + (hours * 60 * 60) + (minutes * 60) + seconds
+
+
+print(f"The amounts of seconds {total_seconds}:")
