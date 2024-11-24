3c40dafbc76bbb8a0e28d8c7ce8eaa199c8c71bd
zahainova <yul.taranenko@gmail.com>
2024-11-24 12:33:39 +0200

    added main

---
+++ lecture-02/exercises/04.py
@@ -0,0 +1,5 @@
+str1 = input("Введіть рядок: ")
+print(str1[len(str1) // 2 - 1] + str1[len(str1) // 2] + str1[len(str1) // 2 + 1])
+
+str2 = input("Введіть рядок: ")
+print(str2[len(str2) // 2 - 1] + str2[len(str2) // 2] + str2[len(str2) // 2 + 1])
