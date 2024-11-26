d2cbb5d616195d2fbb7f957c9da922ee4cfd8019
zahainova <yul.taranenko@gmail.com>
2024-11-26 16:57:13 +0200

    added main

--- lecture-02/exercises/01.py
+++ lecture-02/exercises/01.py
@@ -1,3 +1,3 @@
-str1 = input("Рядок:")
+str1 = "JhonDipPeta"
 
 print(str1[0] + str1[len(str1) // 2] + str1[-1])
---
+++ lecture-02/exercises/03.py
@@ -0,0 +1,6 @@
+word = input()
+
+if str(word) == str(word)[: : -1]:
+    print("The entered string is palindrome")
+else:
+    print("The entered string is not palindrome")
