473feefac043b65a4face8d8ac227fd22b51e9d2
zahainova <yul.taranenko@gmail.com>
2024-11-28 19:30:59 +0200

    added main

--- contacts/main.py
+++ contacts/main.py
@@ -1,12 +1,103 @@
 # main module
 
-contacts = []
+contacts = [] #list
 
-print(contacts)
-print(type (contacts))
-print(id (contacts))
+contact = {
+    'first_name': "john",
+    'last_name': "doe",
+    'phone_number': "1234567"
+}
 
-print(dir (contacts))
 
-print(len(contacts))
+#print(contact)
+#print(type(contact))
+#print(id(contact))
 
+#print(dir(contact))
+
+#print(len(contact))
+
+contacts.append(contact)
+
+TITLE = "phone book"
+def hi():
+    print(f"Hi! It's me, {TITLE.upper()}")
+
+def bye():
+    print(f"Thanks for using {TITLE}")
+
+def your_choice():
+    return input(f"Please make Your choice (l, a, u, r, h, or q)>>>")
+
+def help_me():
+    print("""
+    All that You can do:
+          l: List existing contacts
+          a: Add new contact
+          u: Update existing contact
+          r: Remove existing contact
+          h: Print this help
+          q: Exit
+          """)
+
+def contact_list():
+    if len(contacts) > 0:
+        for item in contacts:
+            for k, v in item.items():
+                print(k, ' => ', v)
+    else:
+        print("Your contact list is empty. Go back to menu and add new contact ")
+
+def add_contact():
+    contact = {}
+    first_name = input("Enter first name:").strip().lower()
+    last_name = input("Enter last name:").strip().lower()
+    phone_number = input("Enter phone number:").strip()
+    contact['first_name'] = first_name
+    contact['last_name'] = last_name
+    contact['phone_number'] = phone_number
+    return contact
+
+def lookup_contact(name):
+    first_name = ''
+    last_name = ""
+    words = name.split()
+    if len(words) == 2:
+        first_name, last_name = words
+    elif len(words) == 1:
+        first_name = words[0]
+    for d in contacts:
+        if d['first_name'] == first_name.lower() and d['last_name'] == last_name.lower():
+            return d
+        elif d['first_name'] == first_name.lower() and last_name == '':
+            return d
+
+def remove_contact(contact):
+    index = contacts.index(contact)
+    confirm = input("Are You sure You want to delete this contact? (y/n): ").strip()
+    if confirm.lower() in ('yes', 'y'):
+        contacts.pop(index)
+
+def main():
+    hi()
+
+    while True:
+       match your_choice():
+           case 'a':
+               contacts.append(add_contact())  
+           case 'l':
+               contact_list()
+           case 'u':
+               pass
+           case 'r':
+               name = input("What name You looking for:")
+               contact = lookup_contact(name)
+               #print(contact)
+               remove_contact(contact)
+           case 'q':
+               bye()
+               break
+           case _:
+               help_me()
+
+main()
--- lecture-02/exercises/01.py
+++ lecture-02/exercises/01.py
@@ -1,3 +1,3 @@
-str1 = "JhonDipPeta"
+str1 = input("Рядок:")
 
 print(str1[0] + str1[len(str1) // 2] + str1[-1])
